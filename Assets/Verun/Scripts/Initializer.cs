213aff37cd6a57381efb4081918c2a7010fe2685
mhelmeck <mhelmecki@gmail.com>
2017-03-25 07:54:43 +0100

    Working on REST

---
+++ Assets/Verun/Scripts/Initializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SimpleJSON;
+using UnityEngine;
+using System.Collections;
+
+
+public struct RoomData
+{
+	public int Id;
+	public int SeatNumber;
+	public ArrayList SeatsReserved;
+}
+
+public class Initializer : MonoBehaviour
+{
+	IEnumerator Start()
+	{
+		string url = "172.20.47.233:8080/api/room";
+		WWW www = new WWW (url);
+		yield return www;
+		if (www.error == null) {
+			ProcessJSONData (www.text);
+		}
+	}
+
+	private void ProcessJSONData(string jsonData) {
+		var parsedJSON = JSON.Parse (jsonData);
+
+	}
+}
