81c02171a25ae0bb6e4f404041ef9d0766c86ce8
Tomasz Błachut <blachut.tomasz@gmail.com>
2017-03-25 06:42:13 +0100

    Mount HUD

---
+++ Assets/Verun/Scripts/Compat.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (SteamVR.instance != null)
+        {
+            Debug.Log("VR detected");
+            enabled = false;
+        }
+        transform.position = new Vector3(0, 1.5f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
