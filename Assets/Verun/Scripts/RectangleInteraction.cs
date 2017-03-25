b81bd686973e4ec8949db8ee26a1d3735d8e69be
Tomasz Błachut <blachut.tomasz@gmail.com>
2017-03-25 01:17:18 +0100

    Test HUD

---
+++ Assets/Verun/Scripts/RectangleInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RectangleInteraction : MonoBehaviour
+{
+
+    private Image panel;
+
+    private void Start ()
+    {
+        panel = GetComponent<Image>();
+    }
+
+    private void Update ()
+	{
+        if (Input.GetButton("Fire1"))
+        {
+            panel.color = Color.yellow;
+        }
+        else
+        {
+            panel.color = Color.white;
+        }
+	}
+}
