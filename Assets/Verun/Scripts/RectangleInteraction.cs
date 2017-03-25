e6f05da36b0513f5fc8f2a91a6852d15f1facca9
Tomasz Błachut <blachut.tomasz@gmail.com>
2017-03-25 01:32:20 +0100

    Modify HUD script

--- Assets/Verun/Scripts/RectangleInteraction.cs
+++ Assets/Verun/Scripts/RectangleInteraction.cs
@@ -7,21 +7,33 @@ public class RectangleInteraction : MonoBehaviour
 {
 
     private Image panel;
+    private Color current = Color.yellow;
 
     private void Start ()
     {
         panel = GetComponent<Image>();
+//        StartCoroutine(CycleColor());
     }
 
     private void Update ()
 	{
         if (Input.GetButton("Fire1"))
         {
-            panel.color = Color.yellow;
+            panel.color = Color.Lerp(Color.yellow, Color.red, Mathf.PingPong(Time.time, 1));
         }
         else
         {
             panel.color = Color.white;
         }
 	}
+
+    private IEnumerator CycleColor()
+    {
+        while (true)
+        {
+            current = current == Color.yellow ? Color.blue : Color.yellow;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
 }
