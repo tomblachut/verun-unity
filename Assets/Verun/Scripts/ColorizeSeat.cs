c59ee53d5e85f26d7d0ad8d3d1be496022cc2728
Angelika Owczarek <angelika.owczarek@gmail.com>
2017-03-25 16:49:17 +0200

    Colorize seat

---
+++ Assets/Verun/Scripts/ColorizeSeat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorizeSeat : MonoBehaviour {
+
+	public Material green;
+	public Renderer rend;
+
+	void Start() {
+		rend = gameObject.GetComponent<Renderer>();
+		rend.enabled = true;
+	}
+
+	void Update() {
+
+		Material[] mats = rend.materials;
+		mats[0] = green; 
+		GetComponent<Renderer>().materials = mats;
+	}
+}
